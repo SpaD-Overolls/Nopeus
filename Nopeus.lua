e853383b55d9d2ddb2e8b42729b070ecde51af16
SpaD-Overolls <142741667+SpaD-Overolls@users.noreply.github.com>
2024-08-05 13:39:59 +0800

    prepare for patching the nopeus' ui directly into UI definitions
    needed to fix the nil bug

--- Nopeus.lua
+++ Nopeus.lua
@@ -10,28 +10,23 @@
 --- VERSION: 1.1.0
 --- LOADER_VERSION_GEQ: 1.0.0
 
-local setting_tabRef = G.UIDEF.settings_tab
-function G.UIDEF.settings_tab(tab)
-    local setting_tab = setting_tabRef(tab)
-
-    if tab == 'Game' then
-        local speeds = create_option_cycle({label = localize('b_set_gamespeed'), scale = 0.8, options = {0.25, 0.5, 1, 2, 3, 4, 8, 16, 32, 64, 999}, opt_callback = 'change_gamespeed', current_option = (
-            G.SETTINGS.GAMESPEED == 0.25 and 1 or
-            G.SETTINGS.GAMESPEED == 0.5 and 2 or 
-            G.SETTINGS.GAMESPEED == 1 and 3 or 
-            G.SETTINGS.GAMESPEED == 2 and 4 or
-            G.SETTINGS.GAMESPEED == 3 and 5 or
-            G.SETTINGS.GAMESPEED == 4 and 6 or 
-            G.SETTINGS.GAMESPEED == 8 and 7 or 
-            G.SETTINGS.GAMESPEED == 16 and 8 or 
-            G.SETTINGS.GAMESPEED == 32 and 9 or 
-            G.SETTINGS.GAMESPEED == 64 and 10 or 
-            G.SETTINGS.GAMESPEED == 999 and 11 or 
-            3 -- Default to 1 if none match, adjust as necessary
-        )})
-        setting_tab.nodes[1] = speeds
-    end
-    return setting_tab
+function G.UIDEF.nopeus_options()
+	local speeds = create_option_cycle({label = localize('b_set_gamespeed'), scale = 0.8, options = {0.25, 0.5, 1, 2, 3, 4, 8, 16, 32, 64, 999}, opt_callback = 'change_gamespeed', current_option = (
+		G.SETTINGS.GAMESPEED == 0.25 and 1 or
+		G.SETTINGS.GAMESPEED == 0.5 and 2 or 
+		G.SETTINGS.GAMESPEED == 1 and 3 or 
+		G.SETTINGS.GAMESPEED == 2 and 4 or
+		G.SETTINGS.GAMESPEED == 3 and 5 or
+		G.SETTINGS.GAMESPEED == 4 and 6 or 
+		G.SETTINGS.GAMESPEED == 8 and 7 or 
+		G.SETTINGS.GAMESPEED == 16 and 8 or 
+		G.SETTINGS.GAMESPEED == 32 and 9 or 
+		G.SETTINGS.GAMESPEED == 64 and 10 or 
+		G.SETTINGS.GAMESPEED == 999 and 11 or 
+		3 -- Default to 1 if none match, adjust as necessary
+	)})
+	
+	return speeds
 end
 
 function Event:init(config)
